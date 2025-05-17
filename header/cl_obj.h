f83c2c3f3b5f7dd698cefa039a784594dcb169e4
vsespontanno <141348664+vsespontanno@users.noreply.github.com>
2025-05-17 15:43:48 +0300

    cl_obj.h

---
+++ header/cl_obj.h
@@ -0,0 +1,12 @@
+#ifndef __CL_OBJ__H
+#define __CL_OBJ__H
+#include "cl_base.h"
+#include <iostream>
+using namespace std;
+
+class cl_obj : public cl_base{ 										              // Наследуем метода базового класса
+	public:
+		cl_obj ( cl_base * p_head_object, string S_object_name ); 	// Конструктор создает корневой объект
+};
+
+#endif
