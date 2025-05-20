c38f08aee5b32ad89230caf253f473633d181b08
vsespontanno <141348664+vsespontanno@users.noreply.github.com>
2025-05-20 22:06:35 +0300

    Update cl_obj.h

--- header/cl_obj.h
+++ header/cl_obj.h
@@ -4,9 +4,9 @@
 #include <iostream>
 using namespace std;
 
-class cl_obj : public cl_base{ 										              // Наследуем метода базового класса
+class cl_obj : public cl_base{ 										             	// Наследуем метода базового класса
 	public:
-		cl_obj ( cl_base * p_head_object, string S_object_name ); 	// Конструктор создает корневой объект
+		cl_obj ( cl_base * p_head_object, string S_object_name );						// Конструктор создает корневой объект
 };
 
 #endif
